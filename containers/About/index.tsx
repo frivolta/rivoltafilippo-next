6b9c351fad090f38acf028281547ad972562605f
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-04-07 17:10:40 +0200

    Adding latest project

---
+++ containers/About/index.tsx
@@ -0,0 +1,91 @@
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoIosDownload,
+  IoIosAt,
+} from "react-icons/io"
+import {
+  AboutWrapper,
+  AboutPageTitle,
+  AboutDetails,
+  SocialProfiles,
+} from "./style"
+import Button from "../../components/Button/button"
+import SocialProfile from "../../components/SocialProfile/socialProfile"
+
+const SocialLinks = [
+  {
+    icon: <IoLogoInstagram />,
+    url: "https://www.instagram.com/filippo.jsx/",
+    tooltip: "Instagram",
+  },
+  {
+    icon: <IoLogoLinkedin />,
+    url: "https://www.linkedin.com/in/filippo-rivolta-49b9723b/",
+    tooltip: "Linkedin",
+  },
+]
+interface AboutProps {}
+
+const About: React.FunctionComponent<AboutProps> = (props) => {
+  const downloadLinkUrl = "/downloads/rivoltafilippo-cv-01042021.pdf"
+
+  return (
+    <AboutWrapper>
+      <AboutDetails>
+        <AboutPageTitle>
+          <h2>
+            As a strong believer in <span>Software Craftmanship</span>, I’m
+            always looking for <span>new things to learn</span>, with Google as
+            my teacher and code editor as my notebook.
+          </h2>
+        </AboutPageTitle>
+        <p>
+          Hi, I am a highly motivated <b>Front-end developer</b> and{" "}
+          <b>Ui Designer</b>
+          crafting rich User Experiences with minimal and aesthetically pleasing
+          interfaces located in <b>Milan, Italy</b>. I have experiece in
+          developing websites and web applications based on web standards
+          technologies like HTML, CSS, <b>JavaScript</b> and PHP. I am
+          passionate about web development with a special affinity for{" "}
+          <b>client-side technologies</b>. The web is my life, exploring web
+          technologies and techniques, developing cool web apps, for this
+          reason, I think working in a team is one of the best ways to improve.
+          Anything that can help me expand my knowledge is always welcome. I
+          love spending time on f<b>ixing little details</b> and optimizing web
+          apps. After hours I am a geek, a wakeboarder / snowboarder, also I
+          love to cook and I admire great food.
+        </p>
+        <h3>
+          For more info contact me or download my cv following the links below!
+        </h3>
+        <br />
+        <a href={downloadLinkUrl} target="_blank">
+          <Button
+            className="no-space"
+            title="Download CV (IT)"
+            type="button"
+            iconPosition="left"
+            icon={<IoIosDownload />}
+          />
+        </a>
+        <br />
+        <a href="/contact">
+          <Button
+            title="Contact me"
+            type="button"
+            iconPosition="left"
+            icon={<IoIosAt />}
+          />
+        </a>
+        <SocialProfiles>
+          <SocialProfile items={SocialLinks} />
+        </SocialProfiles>
+      </AboutDetails>
+    </AboutWrapper>
+  )
+}
+
+export default About
---
+++ containers/Project/style.tsx
@@ -0,0 +1,131 @@
+import { themeGet } from "@styled-system/theme-get"
+import styled from "styled-components"
+
+export const ProjectWrapper = styled.div`
+  position: relative;
+  padding: 90px 75px 0 75px;
+  @media (max-width: 990px) {
+    padding: 80px 45px 30px 45px;
+  }
+  @media (max-width: 575px) {
+    padding: 60px 25px 0 25px;
+  }
+`
+
+export const ProjectHeaderWrapper = styled.div`
+  width: 870px;
+  max-width: 100%;
+  margin: 0 auto;
+  h1 {
+    span {
+      color: ${themeGet("colors.primary")};
+    }
+  }
+  h2 {
+    font-size: 30px;
+    font-weight: 700;
+    color: ${themeGet("colors.textColor", "#292929")};
+    line-height: 1.53;
+    margin-bottom: 10px;
+    @media (max-width: 990px) {
+      font-size: 26px;
+    }
+    @media (max-width: 767px) {
+      font-size: 22px;
+      margin-bottom: 10px;
+    }
+  }
+`
+
+export const ProjectPageTitle = styled.div`
+  margin-bottom: 45px;
+  @media (max-width: 767px) {
+    margin-bottom: 30px;
+  }
+  span {
+    color: ${themeGet("colors.primary")};
+  }
+  h2 {
+    font-size: 30px;
+    font-weight: 700;
+    color: ${themeGet("colors.textColor", "#292929")};
+    line-height: 1.53;
+    margin-bottom: 10px;
+    @media (max-width: 990px) {
+      font-size: 26px;
+    }
+    @media (max-width: 767px) {
+      font-size: 22px;
+      margin-bottom: 10px;
+    }
+  }
+`
+
+export const ProjectImage = styled.div`
+  margin-bottom: 90px;
+  @media (max-width: 990px) {
+    margin-bottom: 60px;
+  }
+  @media (max-width: 767px) {
+    margin-bottom: 40px;
+  }
+`
+
+export const ProjectDetails = styled.div`
+  width: 870px;
+  max-width: 100%;
+  margin: 0 auto;
+
+  h2 {
+    font-size: 30px;
+    font-weight: 700;
+    color: ${themeGet("colors.textColor", "#292929")};
+    line-height: 1.53;
+    margin-bottom: 10px;
+    @media (max-width: 990px) {
+      font-size: 26px;
+    }
+    @media (max-width: 767px) {
+      font-size: 22px;
+      margin-bottom: 10px;
+    }
+  }
+`
+export const SocialProfiles = styled.div`
+  margin-top: 60px;
+  position: relative;
+  @media (max-width: 767px) {
+    margin-top: 40px;
+  }
+
+  &:before {
+    content: "";
+    width: 30px;
+    height: 1px;
+    background: #292929;
+    display: block;
+    margin-bottom: 60px;
+    @media (max-width: 767px) {
+      margin-bottom: 40px;
+    }
+  }
+`
+export const ProjectStructureImageWrapper = styled.div`
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  width: 100%;
+`
+
+export const ProjectStructureImage = styled.img`
+  position: relative;
+  display: block;
+  margin: 0 auto;
+  max-width: 600px;
+  @media (max-width: 990px) {
+    max-width: 400px;
+  }
+  @media (max-width: 767px) {
+    max-width: 100%;
+  }
+`
