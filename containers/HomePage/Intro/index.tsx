cec33be92471d1890c4f79a4e98eea4eaa2b5371
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-04-07 16:41:07 +0200

    Adding intro section

---
+++ containers/HomePage/Intro/index.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { IntroWrapper, IntroImage, IntroTitle, Desciption } from "./style"
+import { IoLogoInstagram, IoLogoLinkedin } from "react-icons/io"
+import Button from "../../../components/Button/button"
+import SocialProfile from "../../../components/SocialProfile/socialProfile"
+import Image from "next/image"
+
+type IntroProps = {}
+
+const SocialLinks = [
+  {
+    icon: <IoLogoInstagram />,
+    url: "https://www.instagram.com/filippo.jsx/",
+    tooltip: "Instagram",
+  },
+  {
+    icon: <IoLogoLinkedin />,
+    url: "https://www.linkedin.com/in/filippo-rivolta-49b9723b/",
+    tooltip: "Linkedin",
+  },
+]
+
+const Intro: React.FunctionComponent<IntroProps> = (props) => {
+  const AuthorImage = "/images/author.jpg"
+
+  return (
+    <IntroWrapper>
+      <IntroImage>
+        <Image src={AuthorImage} alt="author" width="210px" height="210px" />
+      </IntroImage>
+      <IntroTitle>
+        Front-end <b>development </b>
+        and Ui Design.
+      </IntroTitle>
+      <Desciption>
+        Hi, I am a highly motivated <b>front-end developer</b> and Ui designer
+        crafting rich <b>User Experiences</b> with minimal and aesthetically
+        pleasing interfaces located in <b>Milan, Italy</b>. I have experience in
+        developing websites and web applications based on web standard
+        technologies like HTML, CSS, <b>JavaScript</b> and PHP. I am passionate
+        about web development with a special affinity for{" "}
+        <b>client-side technologies.</b>
+      </Desciption>
+      <Desciption>
+        <a href="/downloads/rivoltafilippo-cv-01042021.pdf" target="_blank">
+          <Button title="Download CV (IT)" className="no-space margin-top" />
+        </a>
+      </Desciption>
+      <SocialProfile items={SocialLinks} />
+    </IntroWrapper>
+  )
+}
+
+export default Intro
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head"
+import React from "react"
 import Layout from "../components/layout"
+import PersonalBlog from "../containers/HomePage"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <p>pippo</p>
+      <PersonalBlog />
     </Layout>
   )
 }
