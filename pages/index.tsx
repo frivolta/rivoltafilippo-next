985fad09e47ec6114c201fa0055dd922f8dcf2fd
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-04-10 14:09:59 +0200

    removing static paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,18 +30,3 @@ export const getStaticProps = async () => {
     props: { allPosts },
   }
 }
-
-export async function getStaticPaths() {
-  const posts = getAllPosts(["slug"])
-
-  return {
-    paths: posts.map((posts) => {
-      return {
-        params: {
-          slug: posts.slug,
-        },
-      }
-    }),
-    fallback: false,
-  }
-}
