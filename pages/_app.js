c3c8e04cb7d54ceccfc655275743821f2ca89b71
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-04-11 11:02:47 +0200

    Head and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,16 @@ import "../styles/Home.module.css"
 import { ThemeProvider } from "styled-components"
 import { theme } from "../theme"
 import ResetCss from "../components/resetCSS"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Filippo Rivolta, Front-End Developer</title>
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <ResetCss />
       <Component {...pageProps} />
     </ThemeProvider>
