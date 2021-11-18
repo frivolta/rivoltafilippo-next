5e5e5607589d51695a8d000a7ce4d67231471b5e
frivolta <rivoltafilippo@gmail.com>
2021-11-18 22:10:22 +0100

    Improving seo

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,20 @@ import { ThemeProvider } from "styled-components"
 import { theme } from "../theme"
 import ResetCss from "../components/resetCSS"
 import Head from "next/head"
+import { site } from "../config"
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Filippo Rivolta, Front-End Developer</title>
+        <title>{site.siteMetadata.title}</title>
+        <meta property="og:title" content={site.siteMetadata.title} />
+        <meta property="og:description" content={site.siteMetadata.description} />
+        <meta property="og:url" content={site.siteMetadata.siteUrl} />
+        <meta name="description" content={site.siteMetadata.description} />
+        <meta property="og:type" content="website" />
         <link rel="shortcut icon" href="/favicon-32x32.png" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
