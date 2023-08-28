173547f3f5412fa4b1ae416d856afcde56998580
Filippo Rivolta <filipporivolta@MacBook-Pro-di-Filippo.local>
2023-08-28 17:25:27 +0200

    adding smartlook

--- org/pages/_app.tsx
+++ org/pages/_app.tsx
@@ -24,6 +24,19 @@ function CustomApp({ Component, pageProps }: AppProps) {
           `,
           }}
         />
+         <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.smartlook||(function(d) {
+                var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+                var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+                c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+                })(document);
+                smartlook('init', 'cf4f2f9e81d0146eb8e7a21f13ab9b42fd8543a2', { region: 'eu' });
+              `,
+            }}
+         />
         <title>{site.siteMetadata.title}</title>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta property="og:title" content={site.siteMetadata.title} />
