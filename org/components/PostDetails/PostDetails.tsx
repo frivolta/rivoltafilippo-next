bebdfe7f00b6c08884dc093d95af23a75dec41ac
Filippo Rivolta <filipporivolta@MacBook-Pro-di-Filippo.local>
2023-08-28 18:00:00 +0200

    fix syntax highlighting

--- org/components/PostDetails/PostDetails.tsx
+++ org/components/PostDetails/PostDetails.tsx
@@ -108,7 +108,8 @@ const PostDetails: React.FunctionComponent<PostDetailsProps> = ({
             children={markdownContent}
             components={{
               code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || '');
+                const match = /language-(\w+)/.exec(className || '') || "typescript";
+                
                 return !inline && match ? (
                   <SyntaxHighlighter
                     {...props}
